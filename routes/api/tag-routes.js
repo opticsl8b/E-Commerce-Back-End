8ea63b8054f188a8098ee825fa0818ed8eb6b106
George Cheng <sparklyspuds@gmail.com>
2022-03-16 21:23:40 +0800

    Get one ,Create, update, delete endpoints added

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -34,21 +34,59 @@ router.get("/:id", (req, res) => {
     if (!tag) {
       res.status(400).json({
         message: "No tag was found with this id",
-      });return;
-    }res.json(tag)
+      });
+      return;
+    }
+    res.json(tag);
   });
 });
 
 router.post("/", (req, res) => {
   // create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name,
+  }).then((tag) => {
+    res.json(tag);
+  });
 });
 
 router.put("/:id", (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(
+    {
+      tag_name: req.body.tag_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  ).then((tag) => {
+    if (!tag) {
+      res.status(400).json({
+        message: "No tag was found under this id",
+      });
+      return;
+    }
+    res.json(tag);
+  });
 });
 
 router.delete("/:id", (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  }).then((tag) => {
+    if (!tag) {
+      res.status(400).json({
+        message: "No tag was found under this id",
+      });
+      return;
+    }
+    res.json(tag);
+  });
 });
 
 module.exports = router;
